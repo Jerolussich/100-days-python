7356942bb86c11c48dbf62023ed077e4cd86ba2e
Jeronimo Lussich <jerolussich@hotmail.com>
2022-12-20 11:51:16 -0300

    completed password_generator.py

--- day_5/password_generator.py
+++ day_5/password_generator.py
@@ -1,7 +1,24 @@
-import random, string
-from tkinter import *
+#!/usr/bin/python3
+import random
+import string
 
 print("Welcome to the PyPassword Generator\n")
-size = input("how many letters do you want in your password?\n")
-symbols = input("How many symbols do you like?\n")
-numbers = input("How many numbers would you like?\n")
+letters = int(input("how many letters do you want in your password?\n"))
+symbols = int(input("How many symbols do you like?\n"))
+numbers = int(input("How many numbers would you like?\n"))
+password = ""
+numbers_random = ""
+letters_random = ""
+symbols_random = ""
+
+for i in range(numbers):
+    numbers_random += str(random.choice(string.digits))
+for i in range(letters):
+    letters_random += (random.choice(string.ascii_letters))    
+for i in range(symbols):
+    symbols_random += (random.choice(string.punctuation))
+
+password = random.sample(letters_random + numbers_random + symbols_random, k=(letters + symbols + numbers))
+print(''.join(password))
+
+
